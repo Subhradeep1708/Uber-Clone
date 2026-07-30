3593484ccf847a48f608c95fd6d27622956264eb
Subhradeep Sardar <subhradeep1708@gmail.com>
2026-07-30 22:30:38 +0530

    basic setup and user model creation

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,13 @@ dotenv.config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const dbConnect = require('./db/db')
 
+dbConnect()
 
 app.use(cors())
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('hello')
 })
 
